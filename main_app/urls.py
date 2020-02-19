29e72b0b66b8d6ac415d26817ba299dc05cc945b
johnofhyrule <johnroman7@mac.com>
2020-02-19 11:45:10 -0800

    adds posts url paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('', views.index)
+	path('', views.post_list, name = 'post_list'),
+	path('posts/new', views.post_create, name = 'post_create'),
+	path('posts/<int:pk>', views.post_detail, name = 'post_detail'),
+	path('posts/<int:pk>/edit', views.post_edit, name = 'post_edit'),
+	path('posts/<int:pk>/delete', views.post_delete, name = 'post_delete'),
 ]
