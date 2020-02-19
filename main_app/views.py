f82da9c8c83e11909a49036629d34522385af80d
johnofhyrule <johnroman7@mac.com>
2020-02-19 11:37:29 -0800

    adds posts views and creates comment views

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post, Form
 from .forms import PostForm, CommentForm
@@ -21,7 +21,20 @@ def post_create(request):
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save()
-			return redirect('comment_detail', pk = comment.pk)
+			return redirect('comment_detail', pk = post.pk)
+
+	else:
+		form = PostForm()
+	context = {'form' : form, 'header': "Add New Post"}
+	return render(request, 'post_form.html', context)
+
+def post_edit(request, pk):
+	post = Post.objects.get(pk = pk)
+	if request.method == 'POST':
+		form = PostForm(request.POST, instance = post)
+		if form.is_valid():
+			post = form.save()
+			return redirect('post_detail', pk = post.pk)
 
 	else:
 		form = PostForm(instance = post)
@@ -29,4 +42,41 @@ def post_create(request):
 
 def post_delete(request, pk):
 	Post.objects.get(id = pk).delete()
+	return redirect('comment_list')
+
+# -------- Comment views -------- #
+def comment_list(request):
+	comments = Comment.objects.all()
+	return render(request, 'comment_list.html', {'comments' : comments})
+
+def comment_detail(request, pk):
+	comment = Comment.objects(id = pk)
+	return render(request, 'comment_detail.html', {'comment' : comment})
+
+def comment_create(request):
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save()
+			return redirect('comment_detail', pk = comment.pk)
+
+	else:
+		form = CommentForm()
+	context = {'form' : form, 'header' : "Add New Comment"}
+	return render(request, 'comment_form.html', context)
+
+def comment_edit(request, pk):
+	comment = Comment.objects.get(pk = pk)
+	if request.method == 'POST':
+		form = CommentForm(request.POST, instance = comment)
+		if form.is_valid():
+			comment = form.save()
+			return redirect('comment_detail', pk = comment.pk)
+
+	else:
+		form = CommentForm(instance = comment)
+	return render(request, 'comment_form.html', {'form' : form})
+
+def comment_delete(request, pk):
+	Comment.objects.get(id = pk).delete()
 	return redirect('comment_list')
