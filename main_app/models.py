397df8ce644dadaadd8d8479d0dd6b54033220a5
ksrawr <kenneth.surban62@gmail.com>
2020-02-19 10:59:06 -0800

    adds post and comment models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length = 100)
+	context = models.CharField(max_length = 200)
+	photo_url = models.TextField()
+	likes = models.IntegerField(default = 0)
+	dateCreated = models.TimeField(auto_now_add=True)
+	dateUpdated = models.TimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+	def post_relevent(self):
+		return Post.objects.order_by('-dateUpdated')
+
+class Comment(models.Model):
+	context = models.CharField(max_length = 200)
+	post_fk = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.context
+
+	# def update_post(self):
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Comment
+from .forms import SearchForm
 # Create your views here.
 def index(request):
 	return HttpResponse('<h1>Catbook</h1>')
+
+def global_view(request):
+	# if request.method == 'POST':
+	# 	form = SearchForm(request.POST)
+	# 	if form.is_valid():
+	# 		q = form.cleaned_data['query']
+	# 		posts = Post.objects.raw('SELECT * from Post where title likes "%{q}%"')
+	# 		return render(request, 'global_view', {'posts': posts})
+	form = SearchForm()
+	posts = Post.post_relevent(Post.objects.all())
+	for post in posts:
+		print(post)
+		print(post.dateCreated)
+	return render(request, 'global_view.html', {'posts': posts, 'form': form})
