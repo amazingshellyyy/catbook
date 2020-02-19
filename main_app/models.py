32f612fa70b8c6d566f800ce4d990db0349fa8dd
johnofhyrule <johnroman7@mac.com>
2020-02-19 10:51:58 -0800

    creates comment model

--- main_app/models.py
+++ main_app/models.py
@@ -11,3 +11,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    context = models.CharField(max_length = 400)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.context
