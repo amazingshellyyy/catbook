a8e43c5e878339cc8cf66049b36583285796f25d
ksrawr <kenneth.surban62@gmail.com>
2020-02-18 18:01:03 -0800

    fixes typo in django and main app

--- catbook/urls.py
+++ catbook/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main_app import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', include("main_app.urls")),
 ]
