baa80ab39142241c9255c0e22d26ea37b9b88079
johnofhyrule <johnroman7@mac.com>
2020-02-19 10:49:32 -0800

    creates Post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 140)
+    context = models.CharField(max_length = 750)
+    date = models.DateField()
+    image = models.ImageField(uploadto = 'img' default = 'img/none.jpg')
+    likes = models.IntegerField(default = 0)
+    # user = models.ForeignKey(User, on_delete = models.CASCADE, related, related_name = 'Post')
+
+    def __str__(self):
+        return self.title
