7fd08bb5bc609592615b65deee2d943f3da6636f
johnofhyrule <johnroman7@mac.com>
2020-02-20 14:10:19 -0800

    updates comments to posts

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,14 +5,12 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        # fields = ('title', 'context', 'date', 'likes')
         fields = ('title', 'context',)
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        # fields = ('context', 'date')
         fields = ('context',)
 
 class SearchForm(forms.Form):
