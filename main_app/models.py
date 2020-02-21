435f66ab6a495cce5291d4b097b2d8a392de784c
johnofhyrule <johnroman7@mac.com>
2020-02-20 16:19:37 -0800

    adds username to comments field

--- main_app/models.py
+++ main_app/models.py
@@ -33,7 +33,7 @@ class Comment(models.Model):
     context = models.CharField(max_length = 400)
     post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = 'comments')
     user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'comments')
-    date = models.DateField(auto_now_add = True)
+    date = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.context
