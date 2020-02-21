5bcd1eeb23d75d771ab7053aa2c56c3c72fb1978
johnofhyrule <johnroman7@mac.com>
2020-02-21 15:15:24 -0800

    adds profile path for google sign up/login

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from . import views
+from main_app.views import profile
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='social_app/index.html')),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('profile/', profile, name='profile'),
 ]
