8afa063b26a98f2be7e7335776df910970d14c46
ksrawr <kenneth.surban62@gmail.com>
2020-02-23 17:24:33 -0800

    adds 404 page with updated settings for production

--- catbook/settings.py
+++ catbook/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = '4kt@3y_r^o7kh39k%0qh63l99kievir5rf2kyx9h1#g=u5!shb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+hosts = [
+    'localhost'
+]
+
+print(hosts)
+
+ALLOWED_HOSTS = hosts
 
 
 # Application definition
